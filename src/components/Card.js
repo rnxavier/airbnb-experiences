a773c2d1d9cb81a80a33645e17b9c9fc96d483ca
Richelle Xavier <richellenicole.xavierburton@vodafone.com>
2022-03-03 13:44:40 +0000

    Fixed images bug

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,21 @@
 import React from "react"
+import star from "../images/star.png"
 
 const Card = (props) => {
     let badgeText
     if (props.openSpots === 0) {
         badgeText = "SOLD OUT"
-    }
-    else if(props.location === "Online" && props.openSpots > 0) {
+    } else if(props.location === "Online" && props.openSpots > 0) {
         badgeText = "ONLINE"
     }
 
     return (
         <div className="card-div">
-            <img src={window.location.origin + `/images/${props.img}`} className="card-image" alt=""/>
+            <img src={process.env.PUBLIC_URL + `/images/${props.img}`} className="card-image" alt=""/>
             {badgeText && <div className="badge-text">{badgeText}</div>} 
             <div className="card">
                 <div className="card-stats">
-                <img src={window.location.origin + '/images/star.png'} className="star" alt=""/>
+                    <img src={star} className="star" alt=""/>
                     <span className="rating">{props.stats.rating}</span>
                     <span className="grey">({props.stats.reviews}) • </span>
                     <span className="location">{props.location}</span>
